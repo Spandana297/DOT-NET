6f879c54d9d99cfa617be3a44dfc0552f40984e3
Spandana M <Spandana_Mahendra@amat.com>
2021-02-18 15:02:59 +0530

    Day-2--Task(ref variable,for and for each loop)

---
+++ loops/loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            StreamReader text = new StreamReader("C:/Users/Documents\text.txt");
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Documents\text.txt");
+            watch.Start();
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+            watch.Stop();
+            long t1 = watch.ElapsedMilliseconds;
+            
+
+            //=============================================
+          
+            watch.Start();
+            string Count = text.ReadToEnd();
+            for(int i = 0; i < Count.Length; i++)
+            {
+                Console.WriteLine(Count[i]);
+            }
+            watch.Stop();
+
+            long t2 = watch.ElapsedMilliseconds;
+            Console.WriteLine("readind file using for each loop: ms" +t1);
+
+            Console.WriteLine("readind file using for each loop: ms" +t2);
+        }
+    }
+}
---
+++ loops/loops/reference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//ref parameter doesnot pass property
+class reference
+{
+
+   
+    public static void Main()
+    {
+        string str = "Spandana";
+        // before passing to the method
+        Console.WriteLine(str);
+    
+    // as a reference parameter 
+    Display(ref str);
+
+        // after passing to the method
+        Console.WriteLine(str);
+    }
+
+    static void Display(ref string str1)
+    {
+
+       //change parameter
+        str1 = " Hai Spandana Mahendra";
+    }
+}
