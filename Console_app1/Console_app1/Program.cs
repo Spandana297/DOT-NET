552dd115a898dbf790a1e84e133ae0d2564aebb1
Spandana M <Spandana_Mahendra@amat.com>
2021-02-18 14:05:23 +0530

    Day_1- Pgm

---
+++ Console_app1/Console_app1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace Console_app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 45, 85, 07, 35, 78, 29, 19 };
+            StringBuilder s1 = new StringBuilder("Using linear search  ");
+            int key = 7;
+            for(int i =0;i<8;i++)
+            {
+                if(a[i]==key)
+                {
+                    string str = s1.ToString();
+                    Console.Write(str);
+                    Console.WriteLine("key  is found at position "+ (i+1));
+                    
+                }
+               
+            }
+        
+        }
+    }
+}
