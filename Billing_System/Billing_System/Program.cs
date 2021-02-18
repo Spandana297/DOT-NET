d3adae5a3b978ac91384ff1f33e6beb5a7b00304
Spandana M <Spandana_Mahendra@amat.com>
2021-02-18 14:48:35 +0530

    Day-2 --Task(Nullable,Out)

---
+++ Billing_System/Billing_System/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ModelsItem;
+namespace Billing
+{
+    class Program
+    {
+        private static readonly IDictionary<string, int> items;
+
+        static void Main(string[] args)
+        {
+            display dis = new display();
+           List<Class1> itemmod = new List<Class1>();
+            itemmod.Add(new Class1 { ID = "id1", Name = "chair", Cost = 100 });
+            itemmod.Add(new Class1 { ID = "id2", Name = "headset", Cost = 200 });
+
+
+            Console.WriteLine("Hello!");
+
+            dis.DisplayItems(itemmod);
+
+
+        }
+    }
+}
---
+++ Billing_System/Billing_System/display.cs
@@ -0,0 +1,53 @@
+﻿using ModelsItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    class display
+    {
+        public void DisplayItems(List<Class1> items)
+        {
+           
+            Console.WriteLine("The items in the shop are:");
+            foreach (var ele in items)
+            {
+                Console.WriteLine("{0} - $ {1}",
+                          ele.Name, ele.Cost);
+            }
+            Console.WriteLine("Please select one item.");
+            string Selected = Console.ReadLine().ToLower();
+            StringBuilder str = new StringBuilder();
+            str.Append("You have purchased item ");
+            str.Append(Selected);
+            Console.WriteLine(str);
+            Console.WriteLine("Enter quantity:");
+            int Quantity = int.Parse(Console.ReadLine());
+            int? ItemCost = items.FirstOrDefault(c => c.Name == Selected)?.Cost;
+            //var Item = items.FirstOrDefault(c => c.Name == Selected);
+            //string st = Item.Name;
+            //int cost = Item?.Cost??0;
+            int Total = 0;
+            TotalDisplay(ItemCost, Quantity, Selected, out Total);
+            Totalcost(Total);
+        }
+         
+       
+        public void TotalDisplay(int? ItemCost, int Quantity, string Selected, out int Total)
+        { 
+              Total= ItemCost ?? 0 * Quantity;
+            Console.WriteLine("Total Cost: " + Total);
+          
+
+        }
+
+        public void Totalcost( int Total)
+        {
+           double Totalcost =(Total + 0.18*Total);
+            Console.WriteLine("The total cost with GST is:" + Totalcost);
+
+        }
+    }
+}
